8ee29d205cac49edfd7ed7788339037db6b62b5f
Divine Sam <142226107+DivineSamOfficial@users.noreply.github.com>
2024-05-23 14:32:21 +0530

    Update config.py

--- jobs/config.py
+++ jobs/config.py
@@ -1,4 +1,4 @@
 configuration = {
-    "AWS_ACCESS_KEY": "AKIAQ3EGRYL72YZ4JS6M",
-    "AWS_SECRET_KEY": "1Pu82GR5rXMAvQw4Z+0vXpRXWtA4BdNKb71FU6kj",
+    "AWS_ACCESS_KEY": "YourAccessKey",
+    "AWS_SECRET_KEY": "YourSecretKey",
 }
